0895cae3ca6e731cc9bd8c801b556dac9362bd7e
fathimaca369 <fathimacafathimaca152@gmail.com>
2026-03-30 16:26:39 +0530

    Add files via upload

---
+++ pgm16/Graphics/Square.java
@@ -0,0 +1,14 @@
+package Graphics;
+public class Square implements
+GraphicsSq {
+int side;
+public Square(int s) {
+side = s;
+}
+public int perimeter() {
+return 4 * side;
+}
+public int area() {
+return side * side;
+}
+}
